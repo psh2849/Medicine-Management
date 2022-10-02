4b06d4640203c976254fac8da6de75786655ee76
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2022-10-02 23:42:53 +0900

    Chore: Add Request Code Constants for get Photos From Gallery

--- app/src/main/java/com/example/healthcareapp/util/Constants.kt
+++ app/src/main/java/com/example/healthcareapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.healthcareapp.util
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/"
+        const val BASE_URL = "https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/"
         const val SERVICE_KEY = "Ho%2BO79EuOBas%2BL0GqQ26bphsXGYl%2BizgO3z1Iv9jgKyvAc%2B7MYhEh3ydiBzNL0YZiqliG35KYbdXPu8%2F9DWKSQ%3D%3D"
         const val OUTPUT_TYPE = "json"
 
@@ -10,5 +10,7 @@ class Constants {
         const val QUERY_ITEM_NAME = "itemName"
         const val QUERY_TYPE = "type"
         const val QUERY_NUM_OF_ROWS = "numOfRows"
+
+        const val REQUEST_CODE = 100
     }
 }
