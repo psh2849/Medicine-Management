a737347957bd6f1d9f16cf53b2f9b7a20a1fdbed
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2022-10-05 02:02:21 +0900

    Chore: Change Base Url

--- app/src/main/java/com/example/healthcareapp/util/Constants.kt
+++ app/src/main/java/com/example/healthcareapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.healthcareapp.util
 
 class Constants {
     companion object {
-        const val BASE_URL = "https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/"
+        const val BASE_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/"
         const val SERVICE_KEY = "Ho%2BO79EuOBas%2BL0GqQ26bphsXGYl%2BizgO3z1Iv9jgKyvAc%2B7MYhEh3ydiBzNL0YZiqliG35KYbdXPu8%2F9DWKSQ%3D%3D"
         const val OUTPUT_TYPE = "json"
 
